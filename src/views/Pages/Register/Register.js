48483a0bedf97dcef7e6792dfe81c13bba38079b
Rajesh <rajesh.webethics@gmail.com>
2018-07-09 13:55:37 +0530

    Commiting Changes

--- src/views/Pages/Register/Register.js
+++ src/views/Pages/Register/Register.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Card, CardBody, CardFooter, Col, Container, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import { Button, Card, CardBody, CardFooter, Col, Container, Input, InputGroup, InputGroupAddon, InputGroupText, Row, FormGroup, Label } from 'reactstrap';
 
 class Register extends Component {
   render() {
@@ -12,6 +12,14 @@ class Register extends Component {
                 <CardBody className="p-4">
                   <h1>Register</h1>
                   <p className="text-muted">Create your account</p>
+				  <InputGroup className="mb-3">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="icon-user"></i>
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input type="text" placeholder="Fullname" />
+                  </InputGroup>
                   <InputGroup className="mb-3">
                     <InputGroupAddon addonType="prepend">
                       <InputGroupText>
@@ -41,7 +49,26 @@ class Register extends Component {
                       </InputGroupText>
                     </InputGroupAddon>
                     <Input type="password" placeholder="Repeat password" />
-                  </InputGroup>
+                  </InputGroup> 
+				  
+				  <InputGroup className="mb-4"> 
+                    <FormGroup check inline>
+						<Input className="form-check-input" type="radio" id="inline-radio1" name="inline-radios" value="male" checked />
+						<Label className="form-check-label" check htmlFor="inline-radio1"> Male</Label>
+					  </FormGroup>
+					  <FormGroup check inline>
+						<Input className="form-check-input" type="radio" id="inline-radio2" name="inline-radios" value="female" />
+						<Label className="form-check-label" check htmlFor="inline-radio2">Female</Label>
+					  </FormGroup> 
+                  </InputGroup>  
+				  <InputGroup className="mb-4">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="icon-phone"></i>
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input type="text" placeholder="Contact Number" />
+                  </InputGroup> 
                   <Button color="success" block>Create Account</Button>
                 </CardBody>
                 <CardFooter className="p-4">
